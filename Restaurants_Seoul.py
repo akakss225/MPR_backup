497733f4e3a03739f41bcfa4bca3f7f2b68be80b
akakss225 <akakss225@naver.com>
2022-06-23 11:07:18 +0900

    Seoul data for csv

--- Restaurants_Seoul.py
+++ Restaurants_Seoul.py
@@ -19,5 +19,5 @@ wantedColumn.columns = [
     'lat' # 경도
 ]
 
-wantedColumn.to_csv('/Users/sumin/Desktop/sumin/SideProject/MPR_backup/modified_data_seoul')
+wantedColumn.to_csv('/Users/sumin/Desktop/sumin/SideProject/MPR_backup/modified_data_seoul.csv')
 print(wantedColumn)
