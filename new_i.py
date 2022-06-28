8102a8d4b4b90287df601f08bbce3174a9a71f27
akakss225 <akakss225@naver.com>
2022-06-28 16:25:20 +0900

    데이터 전처리

---
+++ new_i.py
@@ -0,0 +1,28 @@
+from haversine import haversine as hs
+import numpy as np
+import pandas as pd
+
+# 역정보
+stations = pd.read_csv("stations.csv")
+
+# seoul 음식점 정보
+restaurants = pd.read_csv("incheon_data.csv")
+# new
+df = pd.DataFrame()
+
+for r_rowIndex, r_row in restaurants.iterrows():
+    r_location = (r_row.loc['lon'], r_row.loc['lat'])
+    
+    for rowIndex, row in stations.iterrows():
+        st = row.loc['subwayLocation'] + "역" # 지하철 역
+        st_location = (row.loc['lon'], row.loc['lat'])
+        
+        distance = round(hs(r_location, st_location), 3)
+        
+        if r_row.loc['distance'] > distance:
+            r_row.loc['station'] = st
+            r_row.loc['distance'] = distance
+    df = df.append(r_row.loc['name':])
+
+df.to_csv('/Users/sumin/Desktop/sumin/SideProject/MPR_backup/incheon.csv')
+print("success")
--- test.py
+++ test.py
@@ -2,17 +2,8 @@ from haversine import haversine as hs
 import numpy as np
 import pandas as pd
 
-# 역정보
-stations = pd.read_csv("stations.csv")
-
-# seoul 음식점 정보
-restaurants = pd.read_csv("seoul_data.csv")
+restaurants = pd.read_csv("seoul.csv")
 
 for i, r in restaurants.iterrows():
-    r.loc['cate_1'] = '주점'
-    print(r)
-    break
-
-for i, r in restaurants.iterrows():
-    print(r.loc['name':])
-    break
+    if r.loc['station'][-1] != "역":
+        r.loc['station'] += "역"
