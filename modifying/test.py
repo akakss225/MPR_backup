770fa74aacaa12ce39a250441b1d8842269e665c
akakss225 <akakss225@naver.com>
2022-07-01 17:06:25 +0900

    2022.07.01 Stations 전처리

--- modifying/test.py
+++ modifying/test.py
@@ -2,14 +2,14 @@ from haversine import haversine as hs
 import numpy as np
 import pandas as pd
 
-restaurants = pd.read_csv("data/seoul.csv")
+stations = pd.read_csv("modifying/stations.csv")
 
 df = pd.DataFrame()
 
-for i, r in restaurants.iterrows():
-    if r.loc['station'][-1] != "역":
-        r.loc['station'] += "역"
-    df = df.append(r.loc['name':])
+for i, r in stations.iterrows():
+    if r.loc['subwayLocation'][-1] != "역":
+        r.loc['subwayLocation'] += "역"
+    df = df.append(r.loc['subwayLocation':])
 
-df.to_csv('data/seoul2.csv')
+df.to_csv('data/stations.csv')
 print("success")
